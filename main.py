3de207b66decf89fd5346442e6cf46754ac2bd81
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-11-08 08:14:59 -0300

    Feat: Saving exercice in sheets

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import requests
+from datetime import datetime
 
 API_KEY = "349637de93600a18fc325af8a030a5f1"
 APP_ID = "5e26ed8c"
+sheet_endpoint = "https://api.sheety.co/9d6017465440705aa10311b236898f6c/workoutTracking/workouts"
 
 GENDER = "male"
 WEIGHT_KG = "95"
@@ -10,7 +12,6 @@ AGE = "20"
 
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 exercise_text = input("Tell me which exercises you did: ")
-
 
 headers = {
     "x-app-id": APP_ID,
@@ -27,4 +28,21 @@ parameters = {
 
 response = requests.post(exercise_endpoint, json=parameters, headers=headers)
 result = response.json()
-print(result)
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        "workout": {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+    sheet_response = requests.post(sheet_endpoint, json=sheet_inputs)
+
+    print(sheet_response.text)
+    
